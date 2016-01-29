a0f61f10473bc48af1608659132ef738f5802018
Rupesh Mehta <rupeshm@nvidia.com>
2016-01-29 00:28:04 -0800

    Gallery: initial edit
    - forked from github
    - support for reading images from
    device storage

---
+++ Gallery/app/src/main/java/com/grafixartist/gallery/MainActivity.java
@@ -0,0 +1,121 @@
+package com.grafixartist.gallery;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    GalleryAdapter mAdapter;
+    RecyclerView mRecyclerView;
+
+    ArrayList<ImageModel> data = new ArrayList<>();
+    String TAG = "MainActivity";
+
+    public static String IMGS[] = {
+            "https://images.unsplash.com/photo-1444090542259-0af8fa96557e?q=80&fm=jpg&w=1080&fit=max&s=4b703b77b42e067f949d14581f35019b",
+            "https://images.unsplash.com/photo-1439546743462-802cabef8e97?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
+            "https://images.unsplash.com/photo-1441155472722-d17942a2b76a?q=80&fm=jpg&w=1080&fit=max&s=80cb5dbcf01265bb81c5e8380e4f5cc1",
+            "https://images.unsplash.com/photo-1437651025703-2858c944e3eb?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
+            "https://images.unsplash.com/photo-1431538510849-b719825bf08b?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
+            "https://images.unsplash.com/photo-1434873740857-1bc5653afda8?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
+            "https://images.unsplash.com/photo-1439396087961-98bc12c21176?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
+            "https://images.unsplash.com/photo-1433616174899-f847df236857?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
+            "https://images.unsplash.com/photo-1438480478735-3234e63615bb?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
+            "https://images.unsplash.com/photo-1438027316524-6078d503224b?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300"
+    };
+
+    public ArrayList<String> getImageIds(){
+
+        Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = { MediaStore.Images.Media.BUCKET_DISPLAY_NAME, MediaStore.Images.Media.DATA };
+
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+
+        ArrayList<String> ids = new ArrayList<String>();
+        int count = cursor.getCount();
+        int i = 0;
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                int columnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+                /*
+                Log.d(TAG, "ID -------> " + cursor.getString(columnIndex));
+                Log.d(TAG, "---------------------------------------------");
+                */
+                ids.add(cursor.getString(columnIndex));
+
+                i++;
+            }
+            cursor.close();
+            Log.d(TAG, "count = " + Integer.toString(count));
+
+        }
+
+        return ids;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<String> ids = getImageIds();
+        for (int i = 0; i < ids.size(); i++) {
+
+            ImageModel imageModel = new ImageModel();
+            imageModel.setName("Image " + i);
+            imageModel.setUrl(ids.get(i));
+            data.add(imageModel);
+
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        mRecyclerView.setHasFixedSize(true);
+
+
+        mAdapter = new GalleryAdapter(MainActivity.this, data);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this,
+                new RecyclerItemClickListener.OnItemClickListener() {
+
+                    @Override
+                    public void onItemClick(View view, int position) {
+
+                        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                        intent.putParcelableArrayListExtra("data", data);
+                        intent.putExtra("pos", position);
+                        startActivity(intent);
+
+                    }
+                }));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+}
